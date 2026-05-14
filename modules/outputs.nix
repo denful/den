ba63db457bb7dcda883d502ecee957aa15bd4355
Jason Bowman <jason@json64.dev>
2026-05-14 13:57:57 -0700

    feat: auto-inject flake-parts resolution into perSystem

--- modules/outputs.nix
+++ modules/outputs.nix
@@ -2,10 +2,12 @@
   lib,
   den,
   inputs,
+  options,
   ...
 }:
 let
   no-flake-parts = !inputs ? flake-parts;
+  has-flake-parts = inputs ? flake-parts && options ? perSystem;
   flakeModule = den.lib.aspects.resolve "flake" (den.lib.resolveEntity "flake" { });
   flake =
     (lib.evalModules {
@@ -19,4 +21,13 @@ in
 {
   imports = lib.optional no-flake-parts inputs.den.flakeOutputs.flake;
   inherit flake;
+}
+// lib.optionalAttrs has-flake-parts {
+  systems = den.systems;
+
+  perSystem = {
+    imports = [
+      (den.lib.aspects.resolve "flake-parts" (den.lib.resolveEntity "flake-parts" { }))
+    ];
+  };
 }
